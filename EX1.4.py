f0fe4d477d95f6aae82e68927769a894b99c7762
Thariq Ahamed <114891365+itzthariq@users.noreply.github.com>
2024-11-06 16:22:07 +0530

    Update EX1.4.py

--- EX1.4.py
+++ EX1.4.py
@@ -1,7 +1,11 @@
-a=int(input())
-b=int(input())
-c=int(input())
-d=a*b*c
-e=a+b+c
-f=d/e
-print(f)
+'''
+Get Input for variable name, score, department
+Get Score for 100;
+Divide 100/10
+'''
+name= input()
+score=int(input())
+dept=input()
+print("My Name is",name)
+print("My Score is",score/10,"/10")
+print("My Dept is",dept)
